c3889dd440350ff7e650bacd3d662e9d3a900188
githubjesse <jesseburke25@hotmail.com>
2017-10-24 12:44:31 -0300

    Preparing method and file for file reading and parsing.

--- app/src/main/java/com/example/w0299104/quizbuilder/QuizActivity.java
+++ app/src/main/java/com/example/w0299104/quizbuilder/QuizActivity.java
@@ -6,7 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class QuizActivity extends AppCompatActivity {
+
+    String rawTextfile = "raw/rawText.txt";
 
     TextView textViewDefinition;
 
@@ -16,6 +24,8 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        readAndParseRawText(rawTextfile);
 
         textViewDefinition = (TextView) findViewById(R.id.textViewDefinition);
 
@@ -51,5 +61,25 @@ public class QuizActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void readAndParseRawText(String txtFile) {
+        try {
+            //Open file to read from
+            InputStream is = new FileInputStream(txtFile);
+
+            //If file has data
+            if(is != null) {
+
+                //Setup file to be read from
+                InputStreamReader iReader = new InputStreamReader(is);
+                BufferedReader buffer = new BufferedReader(iReader);
+
+                String txtLine;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
