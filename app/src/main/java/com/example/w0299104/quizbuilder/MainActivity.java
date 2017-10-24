1201a32bb70e0cf6aed5c376efb23f3644320b0d
githubjesse <jesseburke25@hotmail.com>
2017-10-24 11:11:20 -0300

    Instancing buttons on the two activities. No substance/body to the program yet.

--- app/src/main/java/com/example/w0299104/quizbuilder/MainActivity.java
+++ app/src/main/java/com/example/w0299104/quizbuilder/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.w0299104.quizbuilder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewTitle;
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
     }
 }
--- app/src/main/java/com/example/w0299104/quizbuilder/QuizActivity.java
+++ app/src/main/java/com/example/w0299104/quizbuilder/QuizActivity.java
@@ -2,12 +2,25 @@ package com.example.w0299104.quizbuilder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
+
+    TextView textViewDefinition;
+
+    Button btnAnswer1, btnAnswer2, btnAnswer3, btnAnswer4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        textViewDefinition = (TextView) findViewById(R.id.textViewDefinition);
+
+        btnAnswer1 = (Button) findViewById(R.id.btnAnswer1);
+        btnAnswer2 = (Button) findViewById(R.id.btnAnswer2);
+        btnAnswer3 = (Button) findViewById(R.id.btnAnswer3);
+        btnAnswer4 = (Button) findViewById(R.id.btnAnswer4);
     }
 }
